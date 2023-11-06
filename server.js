e84b667c3bc525e373c8a71ab0feabbfca9c7b8b
mohdshahzil <theshahzil@gmail.com>
2023-11-06 22:52:07 +0530

    express-routes-setup

--- server.js
+++ server.js
@@ -1,31 +1,22 @@
 const express = require("express");
-const messagesController = require("./controllers/messages.controller");
-const friendsController = require("./controllers/friends.controller");
+const friendsRouter = require("./routes/friends.router");
+const messagesRouter = require("./routes/messages.router");
 const app = express();
 
 const PORT = 3000;
-
-
 
 app.use((req, res, next) => {
   const start = Date.now();
   next();
   //actions go here.. (after all the processing is done)
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post("/friends", friendsController.postFriend);
-
-app.get("/friends", friendsController.getFriends);
-
-app.get("/friends/:friendId", friendsController.getFriend);
-
-app.get("/messages", messagesController.getMessages);
-
-app.post("/messages", messagesController.postMessage);
+app.use("/friends", friendsRouter);
+app.use("/messages", messagesRouter);
 
 app.listen(PORT, () => {
   console.log("Server is listening on port 3000");
