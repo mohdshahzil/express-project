34703ade1d0cdaffcb1123e310e1e4584d7c3764
mohdshahzil <theshahzil@gmail.com>
2023-11-07 10:51:53 +0530

    hdbs

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
 function getMessages(req, res) {
-  res.send("from messages");
+  // res.send("from messages");
+
+  res.sendFile(path.join(__dirname, "..", "public","images", "skimountain.jpg"));
 }
 
 function postMessage(req, res) {
